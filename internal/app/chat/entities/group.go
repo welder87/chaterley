b4c1f2a8fcae80511576e909097b624e98626eca
welder87 <25900160+welder87@users.noreply.github.com>
2026-03-06 23:24:37 +0500

    ISSUE-6. Добавил dostrings. Исправил аргументы для NewGroup.

--- internal/app/chat/entities/group.go
+++ internal/app/chat/entities/group.go
@@ -4,18 +4,27 @@ import (
 	"chaterley/internal/app/core"
 )
 
+// Group представляет группу пользователя чата.
+// Отвечает за права доступа. Например, admin.
 type Group struct {
-	id        core.EntityID
-	name      core.Name
+	// id - Идентификатор группы
+	id core.EntityID
+	// name - Наименование группы
+	name core.Name
+	// createdAt - Дата создания группы
 	createdAt core.CreatedAt
+	// updatedAt - Дата обновления группы
 	updatedAt core.UpdatedAt
+	// deletedAt - Дата удаления группы
 	deletedAt core.DeletedAt
 }
 
-func NewGroup(login string, password string) *Group {
+// NewGroup создает новую группу.
+// В дальнейшем должна возвращать ошибку, если какое-то из полей невалидно.
+func NewGroup(name string) *Group {
 	return &Group{
 		id:        core.NewEntityID(),
-		name:      core.NewName(login),
+		name:      core.NewName(name),
 		createdAt: core.NewCreatedAt(),
 	}
 }
--- internal/app/chat/entities/user.go
+++ internal/app/chat/entities/user.go
@@ -4,16 +4,27 @@ import (
 	"chaterley/internal/app/core"
 )
 
+// User представляет пользователя чата.
+// Основная сущность доменной области Chat.
 type User struct {
-	id        core.EntityID
-	groupID   core.EntityID
-	login     core.Login
-	password  core.PasswordHash
+	// id - Идентификатор пользователя
+	id core.EntityID
+	// groupID - Группа пользователя
+	groupID core.EntityID
+	// login - Логин пользователя
+	login core.Login
+	// password - Хеш пароля пользователя
+	password core.PasswordHash
+	// createdAt - Дата создания пользователя
 	createdAt core.CreatedAt
+	// updatedAt - Дата обновления пользователя
 	updatedAt core.UpdatedAt
+	// deletedAt - Дата удаления пользователя
 	deletedAt core.DeletedAt
 }
 
+// NewUser создает нового пользователя.
+// В дальнейшем должна возвращать ошибку, если какое-то из полей невалидно.
 func NewUser(login string, password string) *User {
 	return &User{
 		id:        core.NewEntityID(),
