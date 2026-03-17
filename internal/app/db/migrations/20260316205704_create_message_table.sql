fbfd0a8027907636d3c86e801ca1b5a67d975092
Kornil17 <Dimitra4@mail.ru>
2026-03-17 20:04:59 +0300

    chaterley-15 Доработана миграция для таблицы message.

--- internal/app/db/migrations/20260316205704_create_message_table.sql
+++ internal/app/db/migrations/20260316205704_create_message_table.sql
@@ -4,18 +4,17 @@ SELECT 'up SQL query';
 CREATE TABLE message (
     id TEXT NOT NULL PRIMARY KEY,
     created_at TEXT NOT NULL,
-    created_at TEXT NOT NULL,
     updated_at TEXT,
     deleted_at TEXT,
     author_id TEXT NOT NULL,
-    is_readed INTEGER DEFAULT 0,
+    is_readed BOOLEAN DEFAULT false,
     content TEXT NOT NULL,
     content_type TEXT NOT NULL DEFAULT 'text' CHECK(content_type IN ('text', 'image', 'video'))
 );
 
+
 CREATE INDEX idx_message_content ON message(content);
 CREATE INDEX idx_message_author_id ON message(author_id);
-CREATE INDEX idx_message_created_at ON message(created_at);
 
 
 -- +goose Down
