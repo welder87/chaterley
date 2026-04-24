557f8b531decbb9d4b1ad522d139fa6aa522c8e6
welder87 <25900160+welder87@users.noreply.github.com>
2026-04-24 22:36:15 +0500

    ISSUE-24 Перенесен репозиторий для пользователя.

--- internal/infrastructure/persistence/repositories/user.go
+++ internal/infrastructure/persistence/repositories/user.go
@@ -15,15 +15,71 @@ func NewUserRepository(dbConn *sql.DB) *UserRepository {
 }
 
 func (r *UserRepository) Save(ctx context.Context, entity *user.User) error {
+	entityDTO := entity.ToSnapshot()
+	query := `
+		INSERT INTO user(
+			id,
+			login,
+			password,
+			created_at,
+			updated_at,
+			deleted_at
+		)
+		VALUES(
+			?, ?, ?, ?, ?, ?
+		)
+	`
+	_, err := r.dbConn.ExecContext(
+		ctx,
+		query,
+		entityDTO.ID,
+		entityDTO.Login,
+		entityDTO.Password,
+		entityDTO.CreatedAt,
+		entityDTO.UpdatedAt,
+		entityDTO.DeletedAt,
+	)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (r *UserRepository) Remove(ctx context.Context, entity *user.User) error {
+	entityDTO := entity.ToSnapshot()
+	query := `
+		DELETE FROM USER WHERE id=?
+	`
+	_, err := r.dbConn.ExecContext(
+		ctx,
+		query,
+		entityDTO.ID,
+	)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (r *UserRepository) Get(ctx context.Context, entityID user.UserID) (*user.User, error) {
-	return nil, nil
+	userFromDB := r.dbConn.QueryRowContext(ctx,
+		"SELECT * FROM user WHERE id=?",
+		entityID.String(),
+	)
+
+	var userDTO user.UserSnapshot
+	err := userFromDB.Scan(
+		&userDTO.ID,
+		&userDTO.Login,
+		&userDTO.Password,
+		&userDTO.CreatedAt,
+		&userDTO.UpdatedAt,
+		&userDTO.DeletedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return user.NewUserFromSnapshot(userDTO)
 }
 
 func (r *UserRepository) GetAll(ctx context.Context) ([]*user.User, error) {
